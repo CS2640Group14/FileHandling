f438eb922fc32b71aabaa67ac9a36b9d0f077704
lmaturinoi <luciainiguez@cpp.edu>
2024-11-13 17:30:42 -0800

    .asm file

---
+++ FileHandlinginMIPS.asm
@@ -0,0 +1,11 @@
+#Date: 11/18/2024
+#Names: Lucia Maturino Iniguez and Roman Sailes
+#File Handling in MIPS - Read Existing, Write New
+# To do:
+# 1) Read "gradedItems.txt" 
+# 2) Copy "gradedItems.txt" 
+# 3) Close "gradedItems.txt"
+# 4) Output "gradedItems.txt"
+# 5) Create new .txt file
+
+ 
